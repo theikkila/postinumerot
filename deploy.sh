c7e8f65406a1c2377666d7d704abd6c42f577246
Teemu Heikkilä <teemu@emblica.fi>
2017-06-06 13:47:15 +0300

    Add npm login and git config

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ git add LAST_UPDATED
 git add postcode_map_light.json
 git add postcode_map.json
 git add postcodes.json
-git commit -m "[ci skip] Refresh zipcodes at ${date}"
+git config user.email "teemu@emblica.fi"
+git config user.name "Teemu Heikkilä"
+git commit -m "[ci skip] Refresh zipcodes at `date`"
 npm version patch
 npm publish
 git push
